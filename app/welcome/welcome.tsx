3f466fe96b5ce1567bd98520500954ba01e7ea78
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2025-04-02 11:43:50 +0200

    var color renaming + indentation

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,11 +3,12 @@ import { Link } from "react-router";
 
 // Tableau des sections
 const sections = [
-  { name: "Khimaira", path: "/khimaira", color: "var(--color-secondary)" },
-  { name: "EPs", path: "/eps", color: "var(--color-accent)" },
-  { name: "Shows", path: "/shows", color: "var(--color-secondary)" },
-  { name: "Videos", path: "/videos", color: "var(--color-accent)" },
-  { name: "Contact", path: "/contact", color: "var(--color-secondary)" },
+  { name: "Khimaira", path: "/khimaira", color: "var(--color-green)" },
+  { name: "EPs", path: "/eps", color: "var(--color-beige)" },
+  { name: "Shows", path: "/shows", color: "var(--color-green)" },
+  { name: "Videos", path: "/videos", color: "var(--color-beige)" },
+  { name: "Radio", path: "/radio", color: "var(--color-green)" },
+  { name: "Contact", path: "/contact", color: "var(--color-beige)" },
 ];
 
 const randomOffsets = sections.map(() => Math.floor(Math.random() * 11) + 10);
@@ -23,19 +24,22 @@ export default function Welcome() {
       <div className="relative h-[300vh] w-full overflow-hidden">
         {sections.map((section, index) => (
           <Link
-          key={section.path}
-          to={section.path}
-          className={`absolute ${index % 2 === 0 ? "right-0 translate-x-4" : "left-0 -translate-x-4"} 
-            w-2/4 h-18 flex items-center justify-center text-[var(--color-primary)] text-2xl font-light
-            rounded-sm transform rotate-[5deg] transition-transform duration-300 ease-in-out`}
-          style={{
-            backgroundColor: section.color,
-            top: `${105 + randomOffsets.slice(0, index).reduce((a, b) => a + b, 0)}vh`,
-          }}
-        >
-          {section.name}
-        </Link>
-        
+            key={section.path}
+            to={section.path}
+            className={`absolute ${
+              index % 2 === 0
+                ? "right-0 translate-x-6 md:right-auto md:left-1/2 md:-translate-x-1/2"
+                : "left-0 -translate-x-6 md:left-1/2 md:-translate-x-1/2"
+            } 
+              w-2/4 md:w-[40%] lg:w-[30%] h-18 flex items-center justify-center text-[var(--color-blue)] text-2xl font-extralight
+              rounded-sm transform rotate-[5deg] transition-transform duration-300 ease-in-out`}
+            style={{
+              backgroundColor: section.color,
+              top: `${105 + randomOffsets.slice(0, index).reduce((a, b) => a + b, 0)}vh`,
+            }}
+          >
+            {section.name}
+          </Link>
         ))}
       </div>
     </Page>
