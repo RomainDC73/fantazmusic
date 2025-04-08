1832d9d10fcd9b15fe22470fe5afef348267301a
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2025-04-08 10:41:27 +0200

    Array of EPs complete

--- app/eps/EPs.tsx
+++ app/eps/EPs.tsx
@@ -8,24 +8,37 @@ const eps = [
     title: "Mirrors",
     year: "2023",
     cover: "https://f4.bcbits.com/img/a0940520493_16.jpg",
-    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=2046215295/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/transparent=true/",
-    bandcampLink: "https://fantaz.bandcamp.com/album/premier-ep"
+    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=2046215295/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
+    bandcampLink: "https://fantaz.bandcamp.com/album/mirrors"
   },
   {
-    title: "Mirrors",
-    year: "2023",
-    cover: "https://f4.bcbits.com/img/a0940520493_16.jpg",
-    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=2046215295/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/transparent=true/",
-    bandcampLink: "https://fantaz.bandcamp.com/album/premier-ep"
+    title: "Une Pour Tony",
+    year: "2022",
+    cover: "https://f4.bcbits.com/img/a1173673113_16.jpg",
+    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=1760048097/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
+    bandcampLink: "https://fantaz.bandcamp.com/album/une-pour-tony"
   },
   {
-    title: "Mirrors",
-    year: "2023",
-    cover: "https://f4.bcbits.com/img/a0940520493_16.jpg",
-    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=2046215295/size=small/bgcol=ffffff/linkcol=f0f/artwork=none/transparent=true/",
-    bandcampLink: "https://fantaz.bandcamp.com/album/premier-ep"
+    title: "Hindiriddims",
+    year: "2021",
+    cover: "https://f4.bcbits.com/img/a0261867598_16.jpg",
+    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=1464518373/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
+    bandcampLink: "https://fantaz.bandcamp.com/album/hindiriddims"
   },
-  // Ajoute ici les 3 autres disques
+  {
+    title: "Climax",
+    year: "2018",
+    cover: "https://f4.bcbits.com/img/a2970696188_16.jpg",
+    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=814428733/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
+    bandcampLink: "https://fantaz.bandcamp.com/album/climax"
+  },
+  {
+    title: "Rollout",
+    year: "2016",
+    cover: "https://f4.bcbits.com/img/a2990108807_16.jpg",
+    bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=1338264662/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
+    bandcampLink: "https://fantaz.bandcamp.com/album/roll-out"
+  },
 ];
 
 export default function EPs() {
