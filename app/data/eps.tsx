38b71464ea55e908d434af8fa747961bfa83de57
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2025-04-22 09:29:31 +0200

    tests with different bc players in EPs page

--- app/data/eps.tsx
+++ app/data/eps.tsx
@@ -4,34 +4,34 @@ export const eps = [
       year: "2023",
       cover: "/assets/fantaz_mirrors_ep_cover.jpg",
       bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=2046215295/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
-      listenLink: "https://li.sten.to/fantaz_mirrors"
+      listenLink: "https://li.sten.to/fantaz_mirrors",
     },
     {
       title: "Une Pour Tony",
       year: "2022",
       cover: "assets/fantaz_une_pour_tony_ep_cover.jpg",
       bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=1760048097/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
-      listenLink: "https://li.sten.to/fantaz-une-pour-tony"
+      listenLink: "https://li.sten.to/fantaz-une-pour-tony",
     },
     {
       title: "Hindiriddims",
       year: "2021",
       cover: "assets/fantaz_hindiriddims_ep_cover.jpg",
       bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=1464518373/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
-      listenLink: "https://li.sten.to/fantaz-hindiriddims"
+      listenLink: "https://li.sten.to/fantaz-hindiriddims",
     },
     {
       title: "Climax",
       year: "2018",
       cover: "assets/fantaz_climax_ep_cover.jpg",
       bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=814428733/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
-      listenLink: "https://li.sten.to/fantaz-climax"
+      listenLink: "https://li.sten.to/fantaz-climax",
     },
     {
       title: "Rollout",
       year: "2016",
       cover: "assets/fantaz_rollout_ep_cover.jpg",
       bandcampEmbedUrl: "https://bandcamp.com/EmbeddedPlayer/album=1338264662/size=small/bgcol=ffffff/linkcol=0D1321/artwork=none/transparent=true/",
-      listenLink: "https://li.sten.to/fantaz-rollout"
+      listenLink: "https://li.sten.to/fantaz-rollout",
     },
   ];
