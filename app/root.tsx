d05104b3be04bef4cdffe03e547412aa7f095cfa
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2025-05-19 11:22:45 +0200

    gtag added

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCJ7DM3QQ7"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KCJ7DM3QQ7');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
